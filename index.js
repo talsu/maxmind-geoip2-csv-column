ca7231531d2bbd5bbf13e31508135fbd32ac4bd1
talsu <talsu84@gmail.com>
2018-07-02 22:46:57 +0900

    국가, 지역명 한글화.

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ let parse = require('csv-parse');
 let stringify = require('csv-stringify');
 let transform = require('stream-transform');
 let argv = require('yargs').argv;
+let koreanCountryName = require('./iso_3166-1_alpha-2_korean');
+let koreanRegionName = require('./iso_3166-2_korean');
 
 let geoIP2CityDB = argv.cityDB || 'GeoLite2-City.mmdb';
 let geoIP2AsnDB = argv.asnDB || 'GeoLite2-ASN.mmdb';
@@ -16,6 +18,7 @@ let inFile = argv.in || null; //'escrow_20180627.csv';
 let outFile = argv.out || null; //'escrow_20180627_geoIpData.csv';
 let clientIpColName = argv.ipColName || 'client_ip';
 let delimiter = argv.delimiter || ',';
+let setKoreaName = true;
 
 maxmind.open(geoIP2CityDB, (err, cityLookup) => {
 maxmind.open(geoIP2AsnDB, (err, asnLookup) => {
@@ -39,6 +42,7 @@ maxmind.open(geoIP2AsnDB, (err, asnLookup) => {
     // ip 값 가져옴
     let ip = record[clientIpColumnIndex];
     let geoIpData = getGeoIpData(ip, cityLookup, asnLookup);
+    if (setKoreaName) geoIpData = changeNameForKorean(geoIpData);
     if (isHeader) { // header
       for (let key in geoIpData) record.push(key); // 헤더는 기존 record에 헤더만 추가한다
       isHeader = false; // 해더는 한번만 처리하면 끝
@@ -58,15 +62,29 @@ function getGeoIpData(ip, cityLookup, asnLookup) {
   return {
     countryCode: getPropertySafe(city, ['country', 'iso_code']),
     countryName: getPropertySafe(city, ['country', 'names', 'en']),
-    region:  getPropertySafe(city, ['subdivisions', 0, 'names', 'en']),
+    geonameId: getPropertySafe(city, ['subdivisions', 0, 'geoname_id']),
+    regionCode: getPropertySafe(city, ['subdivisions', 0, 'iso_code']),
+    regionName: getPropertySafe(city, ['subdivisions', 0, 'names', 'en']),
     city: getPropertySafe(city, ['city', 'names', 'en']),
     continent: getPropertySafe(city, ['continent', 'names', 'en']),
+    accuracyRadius: getPropertySafe(city, ['location', 'accuracy_radius']),
     latitude: getPropertySafe(city, ['location', 'latitude']),
     longitude: getPropertySafe(city, ['location', 'longitude']),
+    timeZone: getPropertySafe(city, ['location', 'time_zone']),
     postalCode: getPropertySafe(city, ['postal', 'code']),
     autonomousSystemNumber: getPropertySafe(asn, ['autonomous_system_number']),
     autonomousSystemOrganization: getPropertySafe(asn, ['autonomous_system_organization'])
   };
+}
+
+function changeNameForKorean(geoIpData) {
+  if (!geoIpData) return geoIpData;
+  if (koreanCountryName[geoIpData.countryCode])
+    geoIpData.countryName = koreanCountryName[geoIpData.countryCode];
+  let isoRegionCode = geoIpData.countryCode + '-' + geoIpData.regionCode;
+  if (koreanRegionName[isoRegionCode])
+    geoIpData.regionName = koreanRegionName[isoRegionCode];
+  return geoIpData;
 }
 
 function getPropertySafe(obj, pathArray) {
